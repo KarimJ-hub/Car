c89b7511c8186ce403a51945c04ecd8fb5a3e69c
KarimJ-hub <karim.jisrel.etu@b13p14.fil.univ-lille.fr>
2025-01-10 11:55:21 +0100

    fix issues

--- FTPServer.java
+++ FTPServer.java
@@ -22,22 +22,50 @@ import java.util.*;
 public class FTPServer {
 
     private static final int PORT = 2121;
-    private static final String USERNAME = "miage";
+    private static final String USER = "miage";
     private static final String PASSWORD = "car";
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Serveur FTP démarré sur le port " + PORT);
 
+            Socket client = serverSocket.accept();
+            InputStream in = client.getInputStream();
+            OutputStream out = client.getOutputStream();
+            out.write("220 Service Ready\r\n".getBytes());
+            Scanner scanner = new Scanner(in);
+            boolean isAuthenticated = false;
             while (true) {
-                Socket client = serverSocket.accept();
-                InputStream in = client.getInputStream();
-                OutputStream out = client.getOutputStream();
-                out.write("220 Service Ready\r\n".getBytes());
-                Scanner scanner = new Scanner(in);
                 String str = scanner.nextLine();
                 System.out.println(str);
+                if (!isAuthenticated) {
+                    if (str.startsWith("USER")) {
+                        String user = str.substring(5).trim();
+                        out.write("331 Password required\r\n".getBytes());
+                    }
+                    else if(str.startsWith("PASS")){
+                        String password = str.substring(5).trim();
+                        if (PASSWORD.equals(password)) {
+                            isAuthenticated = true;
+                            out.write("230 User logged in proceed\r\n".getBytes());
+                        }
+                        else {
+                            out.write("530 Login incorrect\r\n".getBytes());
+                        }
+                    }
+                    else if (str.equalsIgnoreCase("QUIT")){
+                        out.write("221 Goodbye\r\n".getBytes());
+                        break;
+                    }
+
+                    else {
+                        out.write("530 Incorrect authentication please login with USER and PASS\r\n".getBytes());
+                    }
+
+                }
+
             }
+         
         } catch (IOException e) {
             e.printStackTrace();
         }
